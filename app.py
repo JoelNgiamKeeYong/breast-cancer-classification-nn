b703be649bb6b3add00aa485797137de15429f87
Joel <joelngiam@yahoo.com.sg>
2025-02-13 19:51:14 +0800

    Made changes

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import tensorflow as tf
 import joblib
 import plotly.express as px
 import plotly.graph_objects as go
+import tensorflow
 from tensorflow import keras
 
 # Page configuration
